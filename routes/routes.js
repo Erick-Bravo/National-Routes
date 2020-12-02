8132709ea503f56b815f4db8565f5269837db649
Thanh Nguyen <thanhnguyenttn@gmail.com>
2020-12-02 12:11:25 -0500

    setup get request to park/id page

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const express = require("express");
 //importing local files
 const db = require("../db/models");
 const { environment } = require("../config");
-const { asyncHandler, csrfProtection } = require("./utiles")
+const { asyncHandler, csrfProtection } = require("./utiles");
 
 
 //defining global variables and helper functions
@@ -14,22 +14,30 @@ const router = express.Router();
     //HOMEPAGE
 router.get("/", csrfProtection, asyncHandler(async (req, res) => {
     const parks = await db.Park.findAll(); //maybe order the list by average rating.
-    res.render('park-list', {title: 'NATIONAL ROUTES', parks, token: req.csrfToken()})
+  res.render('park-list', { title: 'NATIONAL ROUTES', parks, token: req.csrfToken() });
 
 }));
 
 //TEMPORARY CHECKS SESSION
-router.get("/sessionCheck", (req,res) => {
+router.get("/sessionCheck", (req, res) => {
   if (req.session.views) {
-      req.session.views++
-      res.setHeader('Content-Type', 'text/html')
-      res.write('<p>views: ' + req.session.views + '</p>')
-      res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
-      res.end()
-    } else {
-      req.session.views = 1
-      res.end('welcome to the session demo. refresh!')
-    }
-})
+    req.session.views++
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<p>views: ' + req.session.views + '</p>');
+    res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>');
+    res.end();
+  } else {
+    req.session.views = 1
+    res.end('welcome to the session demo. refresh!');
+  };
+});
+
+router.get('/parks/:id(\\d+)', asyncHandler(async (req, res) => {
+  const parkId = req.params.id;
+  const park = await db.Park.findOne(parkId);
+  res.render('park-page', { park });
+
+}));
+
 //exporting router
 module.exports = router;
