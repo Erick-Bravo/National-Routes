954e593fc01ecbe80448c1687068843780846c06
Erick Bravo <erickbrvo@gmail.com>
2020-12-02 10:11:06 -0800

    bcrypt added password hasher

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,24 +1,35 @@
 const express = require("express");
 const router = express.Router()
+const bcrypt = require("bcryptjs")
 
 //importing local files
 const db = require("../db/models");
 const { environment } = require("../config");
 const { asyncHandler, csrfProtection, signUpValidator, validationResult} = require("./utiles");
 
+
+
+
 router.post("/sign-up", csrfProtection, signUpValidator, asyncHandler(async(req, res) => {
 
     const { username, email, password } = req.body
+
+
     const validatorError = validationResult(req)
     if(validatorError.isEmpty()) {
+        const hashedPassword = await bcrypt.hash(password, 10);
         await db.User.create({
             username,
             email,
-            password,
+            password: hashedPassword,
             createdAt: new Date(),
             updatedAt: new Date()
-         });
+        });
+
+
         res.json({}); // Authentication Token to be inserted in the future
+
+
     } else {
         const errors = validatorError.array().map((error) => error.msg);
         res.json({ errors });
