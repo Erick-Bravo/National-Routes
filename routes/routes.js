2ed58b1ecea9b838ffe4548340a84a0b1f03858a
Anna Bullard <annabullard89@gmail.com>
2020-12-02 15:28:00 -0800

    Clean up

--- routes/routes.js
+++ routes/routes.js
@@ -20,11 +20,6 @@ router.get("/", csrfProtection, asyncHandler(async (req, res) => {
 }));
 
 // // MY ROUTES
-// router.get("/my-routes", csrfProtection, asyncHandler(async (req, res) => {
-//     const parks = await db.Park.findAll();
-//     res.render('my-routes', {title: 'MY ROUTES', parks, token: req.csrfToken()})
-
-// }));
 
 router.get("/my-routes", checkAuth, asyncHandler(async (req, res) => {
     const id = parseInt(req.session.auth.userId);
@@ -35,24 +30,8 @@ router.get("/my-routes", checkAuth, asyncHandler(async (req, res) => {
 
     user = await user.toJSON()
     console.log(user.Parks[0])
-    res.render("my-routes", {title: 'MY ROUTES', parks: user.Parks })
+    res.render("my-routes", {title: 'MY ROUTES', parks: user.Parks, user: {userId: user.id, username: user.username} })
 }))
 
-
-
-
-//TEMPORARY CHECKS SESSION
-router.get("/sessionCheck", (req,res) => {
-  if (req.session.views) {
-      req.session.views++
-      res.setHeader('Content-Type', 'text/html')
-      res.write('<p>views: ' + req.session.views + '</p>')
-      res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
-      res.end()
-    } else {
-      req.session.views = 1
-      res.end('welcome to the session demo. refresh!')
-    }
-})
 //exporting router
 module.exports = router;
