885f4d038c45c3007379eea439a376e500ff15a8
Joe Alves <jmalves@appacademy.io>
2020-12-03 08:33:13 -0800

    ye old way

--- public/scripts_cr.js
+++ public/scripts_cr.js
@@ -1,90 +1,91 @@
 window.addEventListener("DOMContentLoaded", () => {
 
 
-// Sign-Up
-    const signUpForm = document.querySelector("#sign-up-form")
-    signUpForm.addEventListener("submit", async(e) => {
-        e.preventDefault();
-
-        let username = document.querySelector("#sign-up-form input[name='username']").value; // CSS selectors (space = a descendant)
-        let email = document.querySelector("#sign-up-form input[name='email']").value;
-        let password = document.querySelector("#sign-up-form input[name='password']").value;
-        let confirmPassword = document.querySelector("#sign-up-form input[name='confirmPassword']").value;
-        let _csrf = document.querySelector("#sign-up-form input[name='_csrf']").value;
-
-        let result = await fetch("/sign-up", {
-            init: {credentials: "same-origin"},
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ username, email, password, confirmPassword, _csrf })
-         })
-
-        result = await result.json();
-
-        if(!result.errors) {
-            location.href = "/my-routes";
-            return;
-        } else {
-            const errorDiv = document.querySelector("#sign-up-form div.errors");
-            errorDiv.innerHTML = "";
-            result.errors.forEach(error => {
-                const div = document.createElement("div");
-                div.innerHTML = error;
-                errorDiv.appendChild(div);
-            })
-        }
-    })
-
-
-//Login
-    const logInForm = document.querySelector("#login")
-    logInForm.addEventListener("submit", async(e) => {
-        e.preventDefault();
-
-        let email = document.querySelector("#login input[name='email']").value;
-        let password = document.querySelector("#login input[name='password']").value;
-        let _csrf = document.querySelector("#sign-up-form input[name='_csrf']").value;
-
-        let result = await fetch("/login", {
-            init: {credentials: "same-origin"},
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ email, password, _csrf })
-        })
-
-        result = await result.json()
-
-        if(!result.errors) {
-            location.href = "/routes"
-        } else {
-            const errorDiv = document.querySelector("#login div.errors");
-            errorDiv.innerHTML = "";
-            result.errors.forEach(error => {
-                const div = document.createElement("div");
-                div.innerHTML = error;
-                errorDiv.appendChild(div);
-            })
-        }
-    })
-
+    // Sign-Up
+        const signUpForm = document.querySelector("#sign-up-form")
+        signUpForm.addEventListener("submit", async(e) => {
+            e.preventDefault();
     
-    const newRouteForm = document.querySelector("#create-new-route")
-    newRouteForm.addEventListener("submit", async(e) => {
-        e.preventDefault();
-
-        let newRoute = document.querySelector("#create-new-route input[name='new-route']").value;
-        let _csrf = document.querySelector("#sign-up-form input[name='_csrf']").value;
-
-        let result = await fetch("/my-routes/add", {
-            init: {credentials: "same-origin"},
-            method: "POST",
-            headers: { "Content-Type": "application/json"},
-            body: JSON.stringify({ newRoute, _csrf})
+            let username = document.querySelector("#sign-up-form input[name='username']").value; // CSS selectors (space = a descendant)
+            let email = document.querySelector("#sign-up-form input[name='email']").value;
+            let password = document.querySelector("#sign-up-form input[name='password']").value;
+            let confirmPassword = document.querySelector("#sign-up-form input[name='confirmPassword']").value;
+            let _csrf = document.querySelector("#sign-up-form input[name='_csrf']").value;
+    
+            let result = await fetch("/sign-up", {
+                init: {credentials: "same-origin"},
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ username, email, password, confirmPassword, _csrf })
+             })
+    
+            result = await result.json();
+    
+            if(!result.errors) {
+                location.href = "/my-routes";
+                return;
+            } else {
+                const errorDiv = document.querySelector("#sign-up-form div.errors");
+                errorDiv.innerHTML = "";
+                result.errors.forEach(error => {
+                    const div = document.createElement("div");
+                    div.innerHTML = error;
+                    errorDiv.appendChild(div);
+                })
+            }
         })
-
-        result = await result.json();
-
+    
+    
+    //Login
+        const logInForm = document.querySelector("#login")
+        logInForm.addEventListener("submit", async(e) => {
+            e.preventDefault();
+    
+            let email = document.querySelector("#login input[name='email']").value;
+            let password = document.querySelector("#login input[name='password']").value;
+            let _csrf = document.querySelector("#sign-up-form input[name='_csrf']").value;
+    
+            let result = await fetch("/login", {
+                init: {credentials: "same-origin"},
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, password, _csrf })
+            })
+    
+            result = await result.json()
+    
+            if(!result.errors) {
+                location.href = "/routes"
+            } else {
+                const errorDiv = document.querySelector("#login div.errors");
+                errorDiv.innerHTML = "";
+                result.errors.forEach(error => {
+                    const div = document.createElement("div");
+                    div.innerHTML = error;
+                    errorDiv.appendChild(div);
+                })
+            }
+        })
+    
+        
+        const newRouteForm = document.querySelector("#create-new-route")
+        newRouteForm.addEventListener("submit", async(e) => {
+            e.preventDefault();
+    
+            let newRoute = document.querySelector("#create-new-route input[name='new-route']").value;
+            let _csrf = document.querySelector("#sign-up-form input[name='_csrf']").value;
+    
+            let result = await fetch("/my-routes/add", {
+                init: {credentials: "same-origin"},
+                method: "POST",
+                headers: { "Content-Type": "application/json"},
+                body: JSON.stringify({ newRoute, _csrf})
+            })
+    
+            result = await result.json();
+    
+        })
+    
+    
     })
-
-
-})
+    
---
+++ routes/authentication.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router()
+const bcrypt = require("bcryptjs")
+
+//importing local files
+const db = require("../db/models");
+const { environment } = require("../config");
+const { asyncHandler, csrfProtection, signUpValidator, validationResult, loginValidators } = require("./utiles");
+
+//Sing-Up
+
+router.post("/sign-up", csrfProtection, signUpValidator, asyncHandler(async(req, res) => {
+
+    const { username, email, password } = req.body
+
+    const validatorError = validationResult(req)
+    if(validatorError.isEmpty()) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await db.User.create({
+            username,
+            email,
+            password: hashedPassword,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        });
+
+
+        res.json({}); // Authentication Token to be inserted in the future
+
+
+    } else {
+        const errors = validatorError.array().map((error) => error.msg);
+        res.json({ errors });
+    }
+}))
+
+router.get("/demo", asyncHandler( async (req, res) => {
+    const user = await db.User.findOne({
+        where: {
+            username: "DemoUser",
+            email: "DemoUser@NatlRoutes.com"
+        }
+    });
+    console.log(user);
+    if (!user) {
+        res.send("Please seed all files");
+    } else {
+        req.session.auth = {userId: user.id, username: user.username};
+        res.redirect('/my-routes');
+    }
+}))
+
+router.get("/logout", asyncHandler( async (req, res) => {
+    delete req.session.auth;
+    res.redirect("/");
+}))
+//Login
+
+router.post("/login",
+    csrfProtection,
+    loginValidators,
+    asyncHandler (async(req, res) => {
+
+        const { email, password, } = req.body
+        let errors = []
+        const validatorErrors = validationResult(req)
+
+        res.json({})
+
+        if(validatorErrors.isEmpty()) {
+
+        } else {
+            errors = validatorErrors.array().map((error) => error.msg)
+            res.json({ errors });
+        }
+        res.render("user-login", { title: "Login", email, errors, token: req.csrfToken() })
+}))
+
+
+
+module.exports = router
